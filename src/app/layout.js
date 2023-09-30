09ae52f4b0a883373cd6343f0afe68390920c042
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2023-09-30 18:21:26 +0530

    shared/common layout done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,13 @@
+import '../../public/assets/vendor/bootstrap/css/bootstrap.min.css'
+import '../../public/assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import '../../public/assets/vendor/boxicons/css/boxicons.min.css'
+import '../../public/assets/vendor/quill/quill.snow.css'
+import '../../public/assets/vendor/quill/quill.bubble.css'
+import '../../public/assets/vendor/remixicon/remixicon.css'
+import '../../public/assets/vendor/simple-datatables/style.css'
+import '../../public/assets/css/style.css'
 import './globals.css'
+
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        
+              
+      </body>
     </html>
   )
 }
